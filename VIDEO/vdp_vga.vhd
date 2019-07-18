c86500343799295db5ba23e0e10c64a94f4fd403
Cray Elliott <MP2E@archlinux.us>
2019-07-17 23:41:57 -0700

    cherrypick vdp_vga.vhd changes from OCM-PLD v3.7.1

--- VIDEO/vdp_vga.vhd
+++ VIDEO/vdp_vga.vhd
@@ -58,38 +58,40 @@
 -------------------------------------------------------------------------------
 -- Memo
 --   Japanese comment lines are starts with "JP:".
---   JP: ï¿½ï¿½ï¿½{ï¿½ï¿½ï¿½ÌƒRï¿½ï¿½ï¿½ï¿½ï¿½gï¿½sï¿½ï¿½ JP:ï¿½ð“ª‚É•tï¿½ï¿½ï¿½éŽ–ï¿½É‚ï¿½ï¿½ï¿½
+--   JP: “ú–{Œê‚ÌƒRƒƒ“ƒgs‚Í JP:‚ð“ª‚É•t‚¯‚éŽ–‚É‚·‚é
 --
 -------------------------------------------------------------------------------
 -- Revision History
 --
--- 29th,October,2006 modified by Kunihiko Ohnaka
---   - Insert the license text.
---   - Add the document part below.
+-- 3rd,June,2018 modified by KdL
+--  - Added a trick to help set a pixel ratio 1:1
+--    on an LED display at 60Hz (not guaranteed on all displays)
 --
--- ?th,August,2006 modified by Kunihiko Ohnaka
---   - Move the equalization pulse generator from
---     vdp.vhd.
+-- 29th,October,2006 modified by Kunihiko Ohnaka
+--  - Inserted the license text
+--  - Added the document part below
+--
+-- ??th,August,2006 modified by Kunihiko Ohnaka
+--  - Moved the equalization pulse generator from vdp.vhd
 --
 -- 20th,August,2006 modified by Kunihiko Ohnaka
---  - Change field mapping algorithm when interlace
---    mode is enabled.
---        even field  -> even line (odd  line is blacK)
---        odd  field  -> odd line  (even line is blacK)
+--  - Changed field mapping algorithm when interlace mode is enabled
+--        even field  -> even line (odd  line is black)
+--        odd  field  -> odd line  (even line is black)
 --
 -- 13th,October,2003 created by Kunihiko Ohnaka
--- JP: VDPï¿½ÌƒRï¿½Aï¿½ÌŽï¿½ï¿½ï¿½Æ•\ï¿½ï¿½ï¿½fï¿½oï¿½Cï¿½Xï¿½Ö‚Ìoï¿½Í‚ï¿½ï¿½Êƒ\ï¿½[ï¿½Xï¿½É‚ï¿½ï¿½ï¿½ï¿½D
+-- JP: VDP‚ÌƒRƒA‚ÌŽÀ‘•‚Æ•\Ž¦ƒfƒoƒCƒX‚Ö‚Ìo—Í‚ð•Êƒ\[ƒX‚É‚µ‚½D
 --
 -------------------------------------------------------------------------------
 -- Document
 --
--- JP: ESE-VDPï¿½Rï¿½A(vdp.vhd)ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½rï¿½fï¿½Iï¿½Mï¿½ï¿½ï¿½ï¿½ï¿½AVGAï¿½^ï¿½Cï¿½~ï¿½ï¿½ï¿½Oï¿½ï¿½
--- JP: ï¿½ÏŠï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Aï¿½bï¿½vï¿½Xï¿½Lï¿½ï¿½ï¿½ï¿½ï¿½Rï¿½ï¿½ï¿½oï¿½[ï¿½^ï¿½Å‚ï¿½ï¿½B
--- JP: NTSCï¿½Íï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½gï¿½ï¿½ï¿½ï¿½15.7KHzï¿½Aï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½gï¿½ï¿½ï¿½ï¿½60Hzï¿½Å‚ï¿½ï¿½ï¿½ï¿½A
--- JP: VGAï¿½Ìï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½gï¿½ï¿½ï¿½ï¿½31.5KHzï¿½Aï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½gï¿½ï¿½ï¿½ï¿½60Hzï¿½Å‚ï¿½ï¿½ï¿½ï¿½A
--- JP: ï¿½ï¿½ï¿½Cï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ù‚Ú”{ï¿½É‚È‚ï¿½ï¿½ï¿½æ‚¤ï¿½Èƒ^ï¿½Cï¿½~ï¿½ï¿½ï¿½Oï¿½É‚È‚ï¿½ï¿½Ü‚ï¿½ï¿½B
--- JP: ï¿½ï¿½ï¿½ï¿½ï¿½ÅAvdpï¿½ï¿½ ntscï¿½ï¿½ï¿½[ï¿½hï¿½Å“ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Aï¿½eï¿½ï¿½ï¿½Cï¿½ï¿½ï¿½ï¿½ï¿½{ï¿½Ì‘ï¿½ï¿½xï¿½ï¿½
--- JP: ï¿½ï¿½ï¿½xï¿½`ï¿½æ‚·ï¿½é‚±ï¿½Æ‚ÅƒXï¿½Lï¿½ï¿½ï¿½ï¿½ï¿½Rï¿½ï¿½ï¿½oï¿½[ï¿½gï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ä‚ï¿½ï¿½Ü‚ï¿½ï¿½B
+-- JP: ESE-VDPƒRƒA(vdp.vhd)‚ª¶¬‚µ‚½ƒrƒfƒIM†‚ðAVGAƒ^ƒCƒ~ƒ“ƒO‚É
+-- JP: •ÏŠ·‚·‚éƒAƒbƒvƒXƒLƒƒƒ“ƒRƒ“ƒo[ƒ^‚Å‚·B
+-- JP: NTSC‚Í…•½“¯ŠúŽü”g”‚ª15.7KHzA‚’¼“¯ŠúŽü”g”‚ª60Hz‚Å‚·‚ªA
+-- JP: VGA‚Ì…•½“¯ŠúŽü”g”‚Í31.5KHzA‚’¼“¯ŠúŽü”g”‚Í60Hz‚Å‚ ‚èA
+-- JP: ƒ‰ƒCƒ“”‚¾‚¯‚ª‚Ù‚Ú”{‚É‚È‚Á‚½‚æ‚¤‚Èƒ^ƒCƒ~ƒ“ƒO‚É‚È‚è‚Ü‚·B
+-- JP: ‚»‚±‚ÅAvdp‚ð ntscƒ‚[ƒh‚Å“®‚©‚µAŠeƒ‰ƒCƒ“‚ð”{‚Ì‘¬“x‚Å
+-- JP: “ñ“x•`‰æ‚·‚é‚±‚Æ‚ÅƒXƒLƒƒƒ“ƒRƒ“ƒo[ƒg‚ðŽÀŒ»‚µ‚Ä‚¢‚Ü‚·B
 --
 
 LIBRARY IEEE;
@@ -159,8 +161,8 @@ ARCHITECTURE RTL OF VDP_VGA IS
     SIGNAL DATADEOUT        : STD_LOGIC;
 
     -- DISP_START_X + DISP_WIDTH < CLOCKS_PER_LINE/2 = 684
-    CONSTANT DISP_WIDTH     : INTEGER := 562;    -- 30 + 512 + 20
-    CONSTANT DISP_START_X   : INTEGER := 120;
+    CONSTANT DISP_WIDTH             : INTEGER := 576;
+    SHARED VARIABLE DISP_START_X    : INTEGER := 684 - DISP_WIDTH - 2;          -- 106
 BEGIN
 
     VIDEOROUT <= DATAROUT  WHEN VIDEOOUTX = '1' ELSE (OTHERS => '0');
@@ -185,6 +187,42 @@ BEGIN
     XPOSITIONW  <=  HCOUNTERIN(10 DOWNTO 1) - (CLOCKS_PER_LINE/2 - DISP_WIDTH - 10);
     EVENODD     <=  VCOUNTERIN(1);
     WE_BUF      <=  '1';
+
+    -- PIXEL RATIO 1:1 FOR LED DISPLAY
+    PROCESS( CLK21M )
+        CONSTANT DISP_START_Y   : INTEGER := 3;
+        CONSTANT PRB_HEIGHT     : INTEGER := 25;
+        CONSTANT RIGHT_X        : INTEGER := 684 - DISP_WIDTH - 2;              -- 106
+        CONSTANT PAL_RIGHT_X    : INTEGER := 87;                                -- 87
+        CONSTANT CENTER_X       : INTEGER := RIGHT_X - 32 - 2;                  -- 72
+        CONSTANT BASE_LEFT_X    : INTEGER := CENTER_X - 32 - 2 - 3;             -- 35
+    BEGIN
+        IF( CLK21M'EVENT AND CLK21M = '1' )THEN
+            IF( (RATIOMODE = "000" OR INTERLACEMODE = '1' OR PALMODE = '1') AND LEGACY_VGA = '1' )THEN
+                -- LEGACY OUTPUT
+                DISP_START_X := RIGHT_X;                                        -- 106
+            ELSIF( PALMODE = '1' )THEN
+                -- 50HZ
+                DISP_START_X := PAL_RIGHT_X;                                    -- 87
+            ELSIF( RATIOMODE = "000" OR INTERLACEMODE = '1' )THEN
+                -- 60HZ
+                DISP_START_X := CENTER_X;                                       -- 72
+            ELSIF( (VCOUNTERIN < 38 + DISP_START_Y + PRB_HEIGHT) OR
+                   (VCOUNTERIN > 526 - PRB_HEIGHT AND VCOUNTERIN < 526 ) OR
+                   (VCOUNTERIN > 524 + 38 + DISP_START_Y AND VCOUNTERIN < 524 + 38 + DISP_START_Y + PRB_HEIGHT) OR
+                   (VCOUNTERIN > 524 + 526 - PRB_HEIGHT) )THEN
+                -- PIXEL RATIO 1:1 (VGA MODE, 60HZ, NOT INTERLACED)
+--              IF( EVENODD = '0' )THEN                                         -- PLOT FROM TOP-RIGHT
+                IF( EVENODD = '1' )THEN                                         -- PLOT FROM TOP-LEFT
+                    DISP_START_X := BASE_LEFT_X + CONV_INTEGER(NOT RATIOMODE);  -- 35 TO 41
+                ELSE
+                    DISP_START_X := RIGHT_X;                                    -- 106
+                END IF;
+            ELSE
+                DISP_START_X := CENTER_X;                                       -- 72
+            END IF;
+        END IF;
+    END PROCESS;
 
     -- GENERATE H-SYNC SIGNAL
     PROCESS( RESET, CLK21M )
