208b3d5f7285b923f022175032327b052978eac5
jackyangantelope <jack@retroremake.co>
2025-12-09 23:13:39 +0800

    Update sys

--- MSX.sv
+++ MSX.sv
@@ -57,6 +57,8 @@ module emu
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
+	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM
@@ -186,6 +188,8 @@ assign BUTTONS   = 0;
 assign VGA_SCALER= 0;
 assign VGA_DISABLE = 0;
 assign HDMI_FREEZE = 0;
+assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 
 `include "build_id.v"
 localparam CONF_STR = {
